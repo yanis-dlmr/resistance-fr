1ef2106941ded74a9809c9ab51aa456530f0d589
ThomasByr <tbouyer2000@gmail.com>
2023-08-25 18:17:14 +0200

    new super class for all cogs

---
+++ src/helper/cog.py
@@ -0,0 +1,27 @@
+import logging
+
+import discord
+from discord.ext import commands
+
+from ..helper import *
+from ..messages import MessageSender, Embedder
+
+__all__ = ['UsefullCog']
+
+
+class UsefullCog(commands.GroupCog):
+
+  def __init__(self, client: commands.AutoShardedBot) -> None:
+    self.client = client
+    self.dispatcher: MessageSender = client.dispatcher
+    self.embed_builder: Embedder = client.embed_builder
+
+    self.log = logging.getLogger(f'cogs.{self.__class__.__name__.lower()}')
+
+  @commands.Cog.listener()
+  async def on_ready(self):
+    self.log.info(f'{self.__class__.__name__} cog loaded !')
+
+  def log_interaction(self, interaction: discord.Interaction):
+    self.log.debug('[%s] %s#%s - %s(%s)', interaction.guild.name, interaction.user.name,
+                   interaction.user.discriminator, interaction.command.name, self.__class__.__name__.lower())
